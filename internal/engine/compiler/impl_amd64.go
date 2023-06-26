88c25e079958bd505f50db901e46a4c17cd957a4
Anuraag Agrawal <anuraaga@gmail.com>
2023-06-26 16:01:09 +0900

    Stub amd64

--- internal/engine/compiler/impl_amd64.go
+++ internal/engine/compiler/impl_amd64.go
@@ -4504,6 +4504,66 @@ func (c *amd64Compiler) compileConstF64(o *wazeroir.UnionOperation) error {
 	return nil
 }
 
+func (c *amd64Compiler) compileAtomicLoad(_ *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicLoad8(_ *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicLoad16(_ *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicStore(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicStore8(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicStore16(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicRMW(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicRMW8(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicRMW16(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicRMWCmpxchg(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicRMW8Cmpxchg(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicRMW16Cmpxchg(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicMemoryWait(o *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicMemoryNotify(_ *wazeroir.UnionOperation) error {
+	return nil
+}
+
+func (c *amd64Compiler) compileAtomicFence(_ *wazeroir.UnionOperation) error {
+	return nil
+}
+
 // compileLoadValueOnStackToRegister implements compiler.compileLoadValueOnStackToRegister for amd64.
 func (c *amd64Compiler) compileLoadValueOnStackToRegister(loc *runtimeValueLocation) {
 	var inst asm.Instruction
